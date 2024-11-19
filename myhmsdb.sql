295970a0702beae982b6f2a7a10895b422926e2e
Abhinav Shimpi <136893204+AbhinavShimpi19@users.noreply.github.com>
2024-11-19 15:03:57 +0530

    Added cursor

--- myhmsdb.sql
+++ myhmsdb.sql
@@ -5,49 +5,36 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `admintb`
---
-
 CREATE TABLE `admintb` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `username` VARCHAR(50) NOT NULL PRIMARY KEY,
+  `password` VARCHAR(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
 -- Dumping data for table `admintb`
---
-
 INSERT INTO `admintb` (`username`, `password`) VALUES
 ('admin', 'admin123');
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `appointmenttb`
---
-
 CREATE TABLE `appointmenttb` (
-  `pid` int(11) NOT NULL,
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(20) NOT NULL,
-  `lname` varchar(20) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `contact` varchar(10) NOT NULL,
-  `doctor` varchar(30) NOT NULL,
-  `docFees` int(5) NOT NULL,
-  `appdate` date NOT NULL,
-  `apptime` time NOT NULL,
-  `userStatus` int(5) NOT NULL DEFAULT 1,
-  `doctorStatus` int(5) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`ID`)
+  `pid` INT(11) NOT NULL,
+  `ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `fname` VARCHAR(20) NOT NULL,
+  `lname` VARCHAR(20) NOT NULL,
+  `gender` VARCHAR(10) NOT NULL,
+  `email` VARCHAR(30) NOT NULL,
+  `contact` VARCHAR(10) NOT NULL,
+  `doctor` VARCHAR(30) NOT NULL,
+  `docFees` INT(5) NOT NULL,
+  `appdate` DATE NOT NULL,
+  `apptime` TIME NOT NULL,
+  `userStatus` INT(5) NOT NULL DEFAULT 1,
+  `doctorStatus` INT(5) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
 -- Dumping data for table `appointmenttb`
---
-
 INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `contact`, `doctor`, `docFees`, `appdate`, `apptime`, `userStatus`, `doctorStatus`) VALUES
 (4, 1, 'Abhinav', 'Shimpi', 'Male', 'abhinavshimpi2005@gmail.com', '9370580164', 'Ganesh', 550, '2024-02-14', '10:00:00', 1, 0),
 (11, 4, 'Shraddha', 'Kapoor', 'Female', 'shraddha@gmail.com', '9768946252', 'ashok', 500, '2024-02-29', '20:00:00', 1, 1),
@@ -55,22 +42,16 @@ INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `contact`
---
-
 CREATE TABLE IF NOT EXISTS `contact` (
-  `name` varchar(30) NOT NULL,
-  `email` text NOT NULL,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(30) NOT NULL,
+  `email` TEXT NOT NULL,
   `contact` VARCHAR(15) NOT NULL,
-  `message` varchar(200) NOT NULL
+  `message` VARCHAR(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
---
 -- Dumping data for table `contact`
---
-
 INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 ('Anu', 'anu@gmail.com', '7896677554', 'Hey Admin'),
 ('Viki', 'viki@gmail.com', '9899778865', 'Good Job, Pal'),
@@ -84,22 +65,17 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `doctb`
---
-
 CREATE TABLE IF NOT EXISTS `doctb` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `spec` varchar(50) NOT NULL,
-  `docFees` int(10) NOT NULL
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `spec` VARCHAR(50) NOT NULL,
+  `docFees` INT(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
 -- Dumping data for table `doctb`
---
-
 INSERT INTO `doctb` (`username`, `password`, `email`, `spec`, `docFees`) VALUES
 ('ashok', 'ashok123', 'ashok@gmail.com', 'General', 500),
 ('arun', 'arun123', 'arun@gmail.com', 'Cardiologist', 600),
@@ -112,82 +88,48 @@ INSERT INTO `doctb` (`username`, `password`, `email`, `spec`, `docFees`) VALUES
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `patreg`
---
 CREATE TABLE IF NOT EXISTS `patreg` (
-  `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(20) NOT NULL,
-  `lname` varchar(20) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL UNIQUE, -- Increased to avoid duplicate emails
-  `contact` varchar(20) NOT NULL, -- Increased length for phone numbers
-  `password` varchar(255) NOT NULL, -- Adjust for secure storage if hashed
-  `cpassword` varchar(255) NOT NULL,
-  PRIMARY KEY (`pid`)
+  `pid` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `fname` VARCHAR(20) NOT NULL,
+  `lname` VARCHAR(20) NOT NULL,
+  `gender` VARCHAR(10) NOT NULL,
+  `email` VARCHAR(50) NOT NULL UNIQUE,
+  `contact` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `cpassword` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
---
 -- Dumping data for table `patreg`
---
-
 INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`) VALUES
 (1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123'),
 (2, 'Alia', 'Bhatt', 'Female', 'alia@gmail.com', '8976897689', 'alia123', 'alia123'),
-(3, 'Shahrukh', 'khan', 'Male', 'shahrukh@gmail.com', '8976898463', 'shahrukh123', 'shahrukh123'),
-(4, 'Kishan', 'Lal', 'Male', 'kishansmart0@gmail.com', '8838489464', 'kishan123', 'kishan123'),
-(5, 'Gautam', 'Shankararam', 'Male', 'gautam@gmail.com', '9070897653', 'gautam123', 'gautam123'),
-(6, 'Sushant', 'Singh', 'Male', 'sushant@gmail.com', '9059986865', 'sushant123', 'sushant123'),
-(7, 'Nancy', 'Deborah', 'Female', 'nancy@gmail.com', '9128972454', 'nancy123', 'nancy123'),
-(8, 'Kenny', 'Sebastian', 'Male', 'kenny@gmail.com', '9809879868', 'kenny123', 'kenny123'),
-(9, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'william123', 'william123'),
-(10, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123'),
-(11, 'Shraddha', 'Kapoor', 'Female', 'shraddha@gmail.com', '9768946252', 'shraddha123', 'shraddha123');
+(3, 'Shahrukh', 'khan', 'Male', 'shahrukh@gmail.com', '8976898463', 'shahrukh123', 'shahrukh123');
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `prestb`
---
-
 CREATE TABLE IF NOT EXISTS `prestb` (
-  `doctor` varchar(50) NOT NULL,
-  `pid` int(11) NOT NULL,
-  `ID` int(11) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `lname` varchar(50) NOT NULL,
-  `appdate` date NOT NULL,
-  `apptime` time NOT NULL,
-  `disease` varchar(250) NOT NULL,
-  `allergy` varchar(250) NOT NULL,
-  `prescription` varchar(1000) NOT NULL
+  `pres_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `doctor` VARCHAR(50) NOT NULL,
+  `pid` INT(11) NOT NULL,
+  `ID` INT(11) NOT NULL,
+  `fname` VARCHAR(50) NOT NULL,
+  `lname` VARCHAR(50) NOT NULL,
+  `appdate` DATE NOT NULL,
+  `apptime` TIME NOT NULL,
+  `disease` VARCHAR(250) NOT NULL,
+  `allergy` VARCHAR(250) NOT NULL,
+  `prescription` VARCHAR(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
--- Dumping data for table `prestb`
---
 
+-- Dumping data for table `prestb`
 INSERT INTO `prestb` (`doctor`, `pid`, `ID`, `fname`, `lname`, `appdate`, `apptime`, `disease`, `allergy`, `prescription`) VALUES
-('Dinesh', 4, 11, 'Kishan', 'Lal', '2020-03-27', '15:00:00', 'Cough', 'Nothing', 'Just take a teaspoon of Benadryl every night'),
-('Ganesh', 2, 8, 'Alia', 'Bhatt', '2020-03-21', '10:00:00', 'Fever', 'Nothing', 'Just take a Panadol every night'),
-('ashok', 11, 4, 'Shraddha', 'Kapoor', '2020-02-29', '20:00:00', 'Cold', 'Nothing', 'Just take Steam everyday'),
-('ashok', 1, 1, 'Abhinav', 'Shimpi', '2020-02-14', '10:00:00', 'Cold', 'Nothing', 'Take a teaspoon of Benadryl every night');
+('Dinesh', 4, 11, 'Kishan', 'Lal', '2020-03-27', '15:00:00', 'Cough', 'Nothing', 'Just take a teaspoon of Benadryl every night');
 
 -- --------------------------------------------------------
 
--- Trigger to Set Default Status on Appointment Insertion
-DELIMITER //
-CREATE TRIGGER before_insert_appointment
-BEFORE INSERT ON appointmenttb
-FOR EACH ROW
-BEGIN
-  SET NEW.userStatus = 1;
-  SET NEW.doctorStatus = 0;
-END;
-//
-DELIMITER ;
-
--- Table to Log Patient Updates
+-- Trigger to Log Changes in Patient Table
 CREATE TABLE IF NOT EXISTS `patreg_log` (
   `log_id` INT AUTO_INCREMENT PRIMARY KEY,
   `pid` INT,
@@ -195,16 +137,37 @@ CREATE TABLE IF NOT EXISTS `patreg_log` (
   `changes_made` TEXT
 );
 
--- Trigger to Log Changes in Patient Table
 DELIMITER //
+
 CREATE TRIGGER after_update_patreg
 AFTER UPDATE ON patreg
 FOR EACH ROW
 BEGIN
   DECLARE changes TEXT;
   SET changes = CONCAT('Updated patient ID ', OLD.pid, ' - Name: ', OLD.fname, ' ', OLD.lname, ' changed to ', NEW.fname, ' ', NEW.lname);
-  
   INSERT INTO patreg_log (pid, changes_made) VALUES (OLD.pid, changes);
 END;
 //
-DELIMITER ;
+
+-- --------------------------------------------------------
+
+-- Cursor-based Stored Procedure
+DELIMITER //
+
+CREATE PROCEDURE ProcessAppointmentsCursor()
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE app_id INT;
+    DECLARE app_pid INT;
+    DECLARE app_name VARCHAR(40);
+    DECLARE app_doctor VARCHAR(30);
+    DECLARE app_date DATE;
+    DECLARE app_time TIME;
+
+    -- Declare cursor to iterate through appointments
+    DECLARE cur CURSOR FOR 
+        SELECT ID, pid, CONCAT(fname, ' ', lname), doctor, appdate, apptime 
+        FROM appointmenttb;
+
+    -- Declare handler for cursor completion
+    DECLARE CONTINUE HANDLER FOR NOT
